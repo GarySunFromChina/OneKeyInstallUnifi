b4fbe80c3df1558c7e145e30caa175277cd873da
Gary <sunglen@qq.com>
2018-06-22 15:36:37 +0800

    V1.0

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50 
+
+echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+sudo apt-get update 
+
+sudo apt-get install -y mongodb-org
+
+sudo apt-get install unifi
+
+
+
+# echo "mongodb-org hold" | sudo dpkg --set-selections
+# echo "mongodb-org-server hold" | sudo dpkg --set-selections
+# echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+# echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+# echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+
+
+
