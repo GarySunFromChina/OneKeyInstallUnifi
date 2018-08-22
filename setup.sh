7d5530a3eba70148c36347f12bbe9b017c0c0e05
GarySunFromChina <36440190+GarySunFromChina@users.noreply.github.com>
2018-08-22 17:50:25 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
 sudo apt-get update 
 
+sudo apt-get install openjdk-8-jdk
+
 sudo apt-get install -y mongodb-org
 
 sudo apt-get install unifi
